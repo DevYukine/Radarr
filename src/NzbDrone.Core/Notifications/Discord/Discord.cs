818cfad5d4c1140135de824bd264dc09b849cc77
Qstick <qstick@gmail.com>
2020-09-20 00:04:57 -0400

    New: Pretty Discord Notifications (Thanks @hotio)

--- src/NzbDrone.Core/Notifications/Discord/Discord.cs
+++ src/NzbDrone.Core/Notifications/Discord/Discord.cs
@@ -1,7 +1,9 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using FluentValidation.Results;
 using NzbDrone.Common.Extensions;
+using NzbDrone.Core.MediaCover;
 using NzbDrone.Core.Notifications.Discord.Payloads;
 using NzbDrone.Core.Validation;
 
@@ -25,10 +27,60 @@ namespace NzbDrone.Core.Notifications.Discord
                               {
                                   new Embed
                                   {
-                                      Description = message.Message,
+                                      Author = new DiscordAuthor
+                                      {
+                                          Name = "Radarr",
+                                          IconUrl = "https://raw.githubusercontent.com/Radarr/Radarr/aphrodite/Logo/256.png"
+                                      },
+                                      Thumbnail = new DiscordImage
+                                      {
+                                          Url = message.Movie.Images.FirstOrDefault(x => x.CoverType == MediaCoverTypes.Poster).Url
+                                      },
+                                      Image = new DiscordImage
+                                      {
+                                          Url = message.Movie.Images.FirstOrDefault(x => x.CoverType == MediaCoverTypes.Fanart).Url
+                                      },
+                                      Url = $"https://www.themoviedb.org/movie/{message.Movie.TmdbId}",
+                                      Description = "Grabbed",
                                       Title = message.Movie.Title,
                                       Text = message.Message,
-                                      Color = (int)DiscordColors.Warning
+                                      Color = (int)DiscordColors.Warning,
+                                      Fields = new List<DiscordField>
+                                      {
+                                          new DiscordField
+                                          {
+                                              Name = "Overview",
+                                              Value = message.Movie.Overview?.Substring(0, Math.Min(message.Movie.Overview.Length, 300))
+                                          },
+                                          new DiscordField
+                                          {
+                                              Name = "Rating",
+                                              Value = message.Movie.Ratings.Value.ToString()
+                                          },
+                                          new DiscordField
+                                          {
+                                              Name = "Genres",
+                                              Value = message.Movie.Genres.Take(3).Join(", ")
+                                          },
+                                          new DiscordField
+                                          {
+                                              Name = "Quality",
+                                              Value = message.Quality.Quality.Name,
+                                              Inline = true
+                                          },
+                                          new DiscordField
+                                          {
+                                              Name = "Group",
+                                              Value = message.RemoteMovie.ParsedMovieInfo.ReleaseGroup,
+                                              Inline = true
+                                          },
+                                          new DiscordField
+                                          {
+                                              Name = "Size",
+                                              Value = BytesToString(message.RemoteMovie.Release.Size),
+                                              Inline = true
+                                          }
+                                      }
                                   }
                               };
             var payload = CreatePayload($"Grabbed: {message.Message}", embeds);
@@ -42,10 +94,60 @@ namespace NzbDrone.Core.Notifications.Discord
                               {
                                   new Embed
                                   {
-                                      Description = message.Message,
+                                      Author = new DiscordAuthor
+                                      {
+                                          Name = "Radarr",
+                                          IconUrl = "https://raw.githubusercontent.com/Radarr/Radarr/aphrodite/Logo/256.png"
+                                      },
+                                      Thumbnail = new DiscordImage
+                                      {
+                                          Url = message.Movie.Images.FirstOrDefault(x => x.CoverType == MediaCoverTypes.Poster).Url
+                                      },
+                                      Image = new DiscordImage
+                                      {
+                                          Url = message.Movie.Images.FirstOrDefault(x => x.CoverType == MediaCoverTypes.Fanart).Url
+                                      },
+                                      Url = $"https://www.themoviedb.org/movie/{message.Movie.TmdbId}",
+                                      Description = "Imported",
                                       Title = message.Movie.Title,
                                       Text = message.Message,
-                                      Color = (int)DiscordColors.Success
+                                      Color = (int)DiscordColors.Success,
+                                      Fields = new List<DiscordField>
+                                      {
+                                          new DiscordField
+                                          {
+                                              Name = "Overview",
+                                              Value = message.Movie.Overview?.Substring(0, Math.Min(message.Movie.Overview.Length, 300))
+                                          },
+                                          new DiscordField
+                                          {
+                                              Name = "Rating",
+                                              Value = message.Movie.Ratings.Value.ToString()
+                                          },
+                                          new DiscordField
+                                          {
+                                              Name = "Genres",
+                                              Value = message.Movie.Genres.Take(3).Join(", ")
+                                          },
+                                          new DiscordField
+                                          {
+                                              Name = "Quality",
+                                              Value = message.MovieFile.Quality.Quality.Name,
+                                              Inline = true
+                                          },
+                                          new DiscordField
+                                          {
+                                              Name = "Group",
+                                              Value = message.MovieFile.ReleaseGroup,
+                                              Inline = true
+                                          },
+                                          new DiscordField
+                                          {
+                                              Name = "Size",
+                                              Value = BytesToString(message.MovieFile.Size),
+                                              Inline = true
+                                          }
+                                      }
                                   }
                               };
             var payload = CreatePayload($"Imported: {message.Message}", embeds);
@@ -59,9 +161,14 @@ namespace NzbDrone.Core.Notifications.Discord
                               {
                                   new Embed
                                   {
+                                      Author = new DiscordAuthor
+                                      {
+                                          Name = "Radarr",
+                                          IconUrl = "https://raw.githubusercontent.com/Radarr/Radarr/aphrodite/Logo/256.png"
+                                      },
                                       Title = healthCheck.Source.Name,
                                       Text = healthCheck.Message,
-                                      Color = healthCheck.Type == HealthCheck.HealthCheckResult.Warning ? (int)DiscordColors.Warning : (int)DiscordColors.Success
+                                      Color = healthCheck.Type == HealthCheck.HealthCheckResult.Warning ? (int)DiscordColors.Warning : (int)DiscordColors.Danger
                                   }
                               };
 
@@ -118,6 +225,20 @@ namespace NzbDrone.Core.Notifications.Discord
             }
 
             return payload;
+        }
+
+        private static string BytesToString(long byteCount)
+        {
+            string[] suf = { "B", "KB", "MB", "GB", "TB", "PB", "EB" }; //Longs run out around EB
+            if (byteCount == 0)
+            {
+                return "0" + suf[0];
+            }
+
+            var bytes = Math.Abs(byteCount);
+            var place = Convert.ToInt32(Math.Floor(Math.Log(bytes, 1024)));
+            var num = Math.Round(bytes / Math.Pow(1024, place), 1);
+            return (Math.Sign(byteCount) * num).ToString() + suf[place];
         }
     }
 }
